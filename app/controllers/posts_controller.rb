8fdec701394f60bf325f54e16028319077bcd363
wichru <wicher.maciej@gmail.com>
2018-08-18 09:53:18 +0200

    create new post controller

---
+++ app/controllers/posts_controller.rb
@@ -0,0 +1,15 @@
+class PostsController < ApplicationController
+  def index
+    @posts = Post.all.order('created_at desc')
+  end
+
+  def new
+    @post = Post.new
+  end
+
+  def create
+    @post = Post.create!(params[:post].permit(:title, :content))
+
+    redirect_to action: 'index'
+  end
+end
